daaa9a8a7abc64aab068b0aff7ebba174bb24293
Lalit Thakare <thakare.lalit@gmail.com>
2020-04-17 13:13:29 +0530

    Rx-js and Observable part-1

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import {Staff} from '../shared/staff';
 import {STAFFS} from '../shared/staffs';
 
+import {Observable, of } from 'rxjs';
+import { delay } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,21 +12,15 @@ export class LeaderService {
 
   constructor() { }
 
-  getLeaders(): Promise<Staff[]>{ 
-      return new Promise(resolve =>{
-        setTimeout(() => resolve(STAFFS), 2000);
-      });
+  getLeaders(): Observable<Staff[]>{ 
+      return of(STAFFS).pipe(delay(2000));
   }
 
-  getLeader(id: string): Promise<Staff>{
-      return new Promise(resolve => {
-        setTimeout(() => resolve(STAFFS.filter((staff) => (staff.id === id))[0]), 2000);
-      });
+  getLeader(id: string): Observable<Staff>{
+      return of(STAFFS.filter((staff) => (staff.id === id))[0]).pipe(delay(2000));
   }
 
-  getFeaturedLeader(): Promise<Staff>{
-      return new Promise(resolve => {
-        setTimeout(() => resolve(STAFFS.filter((staff) => staff.featured)[0]), 2000);
-      });
+  getFeaturedLeader(): Observable<Staff>{
+      return of(STAFFS.filter((staff) => staff.featured)[0]).pipe(delay(2000));
   }
 }
