638113670eda94de138e85d6aaad90f2e10e7a1a
Lalit Thakare <thakare.lalit@gmail.com>
2020-04-18 17:47:35 +0530

    more animation

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import {Staff} from '../shared/staff';
 import {LeaderService} from '../services/leader.service';
+import { flyInOut, expand  } from '../animations/app.animation';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  host: {
+  '[@flyInOut]': 'true',
+  'style': 'display: block;'
+  },
+  animations: [
+    flyInOut(),
+    expand()
+  ]
+
 })
 export class AboutComponent implements OnInit {
 
